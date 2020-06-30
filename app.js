e47a85d978d8b68779bd8a350e97a7511e142c95
Jonathon McCallum <jonmccallum@Jons-MacBook-Pro-2.local>
2020-06-30 14:31:55 +0100

    style and generate names

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser")
-const superheroes = require("superheroes")
-const supervillains = require("supervillains")
+const bodyParser = require('body-parser');
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+const randomFont = require('random-font');
+const _ = require('lodash');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 
 
-app.get("/", function(req, res){
+app.get('/', function(req, res){
 
-    const hero = superheroes.random();
-    const villain = supervillains.random();
-
-    res.render("supers", {hero: hero, villain: villain});
+    const hero = _.upperCase(superheroes.random());
+    const villain = _.upperCase(supervillains.random());
+    const font1 = randomFont();
+    const font2 = randomFont();
+    res.render('supers', {hero: hero, villain: villain, font1: font1, font2: font2});
 });
 
-app.post("/", function(req, res){
-
-    const hero = superheroes.random();
-    const villain = supervillains.random();
-
-    res.render("supers", {hero: hero, villain: villain});
-
-})
 
 app.listen(3000, function(){
-    console.log("listenting on 3000");
+    console.log('listenting on 3000');
 });
 
